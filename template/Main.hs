cfb9d55756bc1ea69fa3a3678eae42f0f9b525e3
Gideon Sireling <code@accursoft.com>
2013-08-01 18:04:57 +0300

    template: show database info

--- template/Main.hs
+++ template/Main.hs
@@ -12,7 +12,13 @@ main = do [host,port] <- getArgs
           inet <- inet_addr host
           bind sock $ SockAddrInet (fromInteger $ read port) inet
           listen sock maxListenQueue
+          (env,_) <- unzip `liftM` getEnvironment
+          let response = "HTTP/1.1 200 OK\r\n\r\nWelcome to Haskell Cloud!\n\n" ++ unlines (zipWith
+                           (\var db -> db ++ if var `elem` env then " is installed on $" ++ var ++ "." else " is not installed.")
+                           ["OPENSHIFT_MYSQL_DB_URL", "OPENSHIFT_MONGODB_DB_URL", "OPENSHIFT_POSTGRESQL_DB_URL"]
+                           ["MySQL", "MongoDB", "PostgreSQL"]) ++
+                         "\nIf you have installed a database that is not recognised, restart the Haskell cartridge."
           forever $ do (handle,_,_) <- Network.accept sock
                        read <- liftM (any (null . dropWhile isSpace) . lines) $ hGetContents handle
-                       when read $ hPutStr handle "HTTP/1.1 200 OK\r\n\r\nWelcome to Haskell Cloud"
+                       when read $ hPutStr handle response
                        hClose handle
