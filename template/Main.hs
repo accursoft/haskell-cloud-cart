58e9cfd6e9bbec5594b8ad7f21157f0bab183b16
Gideon Sireling <code@accursoft.com>
2013-12-22 18:42:33 +0200

    template: list packages

--- template/Main.hs
+++ template/Main.hs
@@ -5,6 +5,7 @@ import Network
 import Network.BSD
 import Network.Socket
 import System.Environment
+import System.Process
 
 main = do [host,port] <- getArgs
           sock <- socket AF_INET Stream defaultProtocol
@@ -12,12 +13,8 @@ main = do [host,port] <- getArgs
           inet <- inet_addr host
           bind sock $ SockAddrInet (fromInteger $ read port) inet
           listen sock maxListenQueue
-          (env,_) <- unzip `liftM` getEnvironment
-          let response = "HTTP/1.1 200 OK\r\n\r\nWelcome to Haskell Cloud!\n\n" ++ unlines (zipWith
-                           (\var db -> db ++ if var `elem` env then " is installed on $" ++ var ++ "." else " is not installed.")
-                           ["OPENSHIFT_MYSQL_DB_URL", "OPENSHIFT_MONGODB_DB_URL", "OPENSHIFT_POSTGRESQL_DB_URL"]
-                           ["MySQL", "MongoDB", "PostgreSQL"]) ++
-                         "\nIf you have installed a database that is not recognised, restart the Haskell cartridge."
+          packages <- readProcess "ghc-pkg" ["list", "--simple-output"] []
+          let response = "HTTP/1.1 200 OK\r\n\r\nWelcome to Haskell Cloud! The following packages are pre-installed:\n\n" ++ unlines (words packages)
           forever $ do (handle,_,_) <- Network.accept sock
                        read <- liftM (any (null . dropWhile isSpace) . lines) $ hGetContents handle
                        when read $ hPutStr handle response
